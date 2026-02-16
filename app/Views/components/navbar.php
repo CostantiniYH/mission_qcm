539d9f392792bf3a251739e4a2091f744d093ba3
CostantiniYH <costantiniyaacov@gmail.com>
2026-02-17 00:15:17 +0100

    Correction nom de colonne table reponse Q en q et R en r

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-opacity-50">
   <div class="container-fluid fs-5">
-    <a class="navbar-brand fs-3" href="/">QCM Yhc</a>
+    <a class="navbar-brand fs-3" href="<?=   BASE_URL ?>">QCM Yhc</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active bi bi-house-fill" aria-current="page" href="/"> Questionnaire</a>
+          <a class="nav-link active bi bi-house-fill" aria-current="page" href="<?=   BASE_URL ?>"> Questionnaire</a>
         </li>
         <!-- <li class="nav-item">
           <a class="nav-link" href="/"></a>
--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -8,7 +8,7 @@
         <div class="footer-section col-md">
           <h4>Liens</h4>
           <ul class="list-unstyled">
-            <li><a class="text-decoration-none" href="/">Accueil</a></li>
+            <li><a class="text-decoration-none" href="<?=   BASE_URL ?>">Accueil</a></li>
             <li><a class="text-decoration-none" href="<?= BASE_URL ?>">Contact</a></li>
             <!-- <li class="list-group-item"><a href="<?= BASE_URL ?>"></a></li> -->
           </ul>
@@ -21,7 +21,7 @@
           <a href="#" class="me-2 text-white bi bi-twitter-x"></a>
         </div>
       </div>
-      <p class="footer-bottom text-center">&copy; <?= date('Y') ?> QCM Entrprise YHC - Tous droits réservés.</p>
+      <p class="footer-bottom text-center">&copy; <?= date('Y') ?> QCM Entreprise YHC - Tous droits réservés.</p>
     </div>
   </footer>
 
