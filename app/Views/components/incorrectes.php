ce0bc099e4d553bebdfdeeca638947ac0aeac68d
CostantiniYH <costantiniyaacov@gmail.com>
2026-03-07 22:56:39 +0100

    traitement des données et affichage logique du qcm

--- app/Views/components/incorrectes.php
+++ app/Views/components/incorrectes.php
@@ -1,8 +1,6 @@
-
-        <div class="p-3 bg-incorrect rounded-4 m-2 w-75 mx-auto shadow-sm">
-            <strong><?= $cpt ?>. A la question : </strong> <?= $ligne2['libelleQ'] . "<br>" ?>
-            <strong>Vous avez répondu : </strong> <?= $ligne['libeller'] . "<br>" ?>
-            <strong>La réponse était :  </strong> <?= $ligne2['libeller'] . "<br>" ?>
-        </div>
-
+<div class="p-3 bg-incorrect rounded-4 m-2 w-75 mx-auto shadow-sm">
+    <strong><?= $cpt ?>. A la question : </strong> <?= $ligne2['libelleq'] . "<br>" ?>
+    <strong>Vous avez répondu : </strong> <?= $ligne['libeller'] . "<br>" ?>
+    <strong>La réponse était :  </strong> <?= $ligne2['libeller'] . "<br>" ?>
+</div>
                    
